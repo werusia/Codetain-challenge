359fcbd750c5e728c6d7443e513bca4135685ec4
Weronika Majewska <weronikaa1@gmail.com>
2019-07-01 09:37:19 +0000

    router.js edited online with Bitbucket

--- src/router.js
+++ src/router.js
@@ -14,10 +14,7 @@ export default new Router({
     {
       path: '/status',
       name: 'status',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/Status.vue')
+      component: () => import('./views/Status.vue')
     }
   ]
 })
