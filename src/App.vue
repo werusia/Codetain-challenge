a1b309b3e417cf115146ca7de23f37e1de3d37b7
Weronika Majewska <weronikaa1@gmail.com>
2019-07-01 09:38:41 +0000

    App.vue edited online with Bitbucket

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/status">Status</router-link>
       </div>
-      All rights reservedâ€¦ or not.
+      All rights reserved… or not.
       <br>
       <a target="_blank" href="https://pl.linkedin.com/in/weronika-majewska-b5b887183">
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
@@ -76,7 +76,7 @@ scroll-behavior:smooth;
 .fade-enter-active, .fade-leave-active {
   transition: opacity .5s;
 }
-.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+.fade-enter, .fade-leave-to {
   opacity: 0;
 }
 </style>
